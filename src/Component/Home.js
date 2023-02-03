4383926177b10e0d7b52e65e12f58d21e8da598c
MN-React-Js-Dev <mayur.nandwalker.5057@gmail.com>
2023-02-03 16:13:24 +0530

    kanda

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -50,7 +50,7 @@ const Home = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <span class="fw-semibold d-block mb-1">Profit</span>
+                                        <span class="fw-semibold d-block mb-1">Orders</span>
                                         <h3 class="card-title mb-2">$12,628</h3>
                                         <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small>
                                     </div>
@@ -74,7 +74,7 @@ const Home = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <span>Sales</span>
+                                        <span>In Production</span>
                                         <h3 class="card-title text-nowrap mb-1">$4,679</h3>
                                         <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
                                     </div>
@@ -154,7 +154,7 @@ const Home = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <span class="d-block mb-1">Payments</span>
+                                        <span class="d-block mb-1">Shiped</span>
                                         <h3 class="card-title text-nowrap mb-2">$2,456</h3>
                                         <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
                                     </div>
@@ -178,7 +178,7 @@ const Home = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <span class="fw-semibold d-block mb-1">Transactions</span>
+                                        <span class="fw-semibold d-block mb-1">Deliverd</span>
                                         <h3 class="card-title mb-2">$14,857</h3>
                                         <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.14%</small>
                                     </div>
