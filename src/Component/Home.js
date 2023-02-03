f22868aab353d64da386413c7aa6b37d21be865e
MN-React-Js-Dev <mayur.nandwalker.5057@gmail.com>
2023-02-03 16:07:02 +0530

    Removed unnessesary code

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -184,7 +184,7 @@ const Home = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>                        
+                        </div>
                     </div>
                 </div>
                 <div class="row">
