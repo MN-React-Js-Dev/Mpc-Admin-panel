0a64eec6b45fd91afe072b555ec0cfefb7798b34
MN-React-Js-Dev <mayur.nandwalker.5057@gmail.com>
2023-02-03 11:51:24 +0530

    new chnages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Component/Footer';
+import Header from './Component/Navbar/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+          <Header />
+          {/* <Footer/> */}
+        </div>
+      </div>
+    </>
   );
 }
 
