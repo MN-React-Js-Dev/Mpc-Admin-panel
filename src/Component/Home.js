327842c24819ada572eb60dc28edb4f523b633d7
sejal-amancha <amancha.sejal.5057@gmail.com>
2023-03-30 16:21:44 +0530

    feature/home

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,35 +1,41 @@
-import React from 'react'
+import React from "react";
+import image from "../../src/assets/img/backgrounds/Designer-dupatta.jpg";
 
 const Home = () => {
-    return (
-        <>
-            <div class="container-xxl flex-grow-1 container-p-y">
-                <div class="row">
-                    <div class="col-lg-8 mb-4 order-0">
-                        <div class="card">
-                            <div class="d-flex align-items-end row">
-                                <div class="col-sm-7">
-                                    <div class="card-body">
-                                        <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
-                                        <p class="mb-4">
-                                            You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
-                                            your profile.
-                                        </p>
+  return (
+    <>
+      <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+          <div class="col-lg-12 mb-4 order-0">
+            <div class="card">
+              <div class="d-flex align-items-end row">
+                <div class="col-sm-7">
+                  <div class="card-body">
+                    <h5 class="card-title text-primary">Congratulations! 🎉</h5>
+                    <p class="mb-4">
+                      You have done <span class="fw-bold">72%</span> more sales
+                      today.
+                      {/* Check your new badge in  your profile. */}
+                    </p>
 
-                                        <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
-                                    </div>
-                                </div>
-                                <div class="col-sm-5 text-center text-sm-left">
-                                    <div class="card-body pb-0 px-0 px-md-4">
-                                        <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140"
-                                            alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png"
-                                            data-app-light-img="illustrations/man-with-laptop-light.png" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 order-1">
+                    {/* <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a> */}
+                  </div>
+                </div>
+                <div class="col-sm-5 text-center text-sm-left">
+                  <div class="card-body pb-0 px-0 px-md-4">
+                    <img
+                      src="../assets/img/illustrations/man-with-laptop-light.png"
+                      height="140"
+                      alt="View Badge User"
+                      data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                      data-app-light-img="illustrations/man-with-laptop-light.png"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <div class="col-lg-4 col-md-4 order-1">
                         <div class="row">
                             <div class="col-lg-6 col-md-12 col-6 mb-4">
                                 <div class="card">
@@ -81,9 +87,9 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
-                    <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
+          {/* <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
                         <div class="card">
                             <div class="row row-bordered g-0">
                                 <div class="col-md-8">
@@ -132,9 +138,9 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
-                    <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
+          {/* <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
                         <div class="row">
                             <div class="col-6 mb-4">
                                 <div class="card">
@@ -185,9 +191,19 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
+                    </div> */}
+        </div>
+            <div class="row">
+                <div class="col-lg-12 mb-4 order-0">
+                    <div class="card">
+                        <div class="d-flex align-items-center">
+                            <img src={image} width="50%" />
+                        </div>
                     </div>
                 </div>
-                <div class="row">
+            </div>
+
+        {/* <div class="row">
                     <div class="col-md-6 col-lg-4 col-xl-4 order-0 mb-4">
                         <div class="card h-100">
                             <div class="card-header d-flex align-items-center justify-content-between pb-0">
@@ -440,10 +456,10 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </>
-    )
-}
+                </div> */}
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
