78ec5bf59c35bc8651f3bbebedf467f0dcf806a7
sejal-amancha <amancha.sejal.5057@gmail.com>
2023-04-04 16:37:29 +0530

    live-url

--- src/Redux/APIs/ordersApi.js
+++ src/Redux/APIs/ordersApi.js
@@ -10,42 +10,23 @@ const headersParam1 = {
     "Authorization" : `Bearer ${tracker}`,
 }; 
 
-// export const loadOrdersApi = async () => await axios.get(`https://api-v1.niceishfabrics.com/api/order/`, { headers: headersParam });
+export const loadOrdersApi = async () => await axios.get(`https://api-v1.niceishfabrics.com/api/order/`, { headers: headersParam });
 
-// export const createOrdersApi = async (order) => await axios.post(`https://api-v1.niceishfabrics.com/api/order/Create`, order, { headers: headersParam });
+export const createOrdersApi = async (order) => await axios.post(`https://api-v1.niceishfabrics.com/api/order/Create`, order, { headers: headersParam });
 
-// export const updateOrderApi = async (order) => await axios.put(`https://api-v1.niceishfabrics.com/api/order/${order.get('id')}`, order, { headers : headersParam });
+export const updateOrderApi = async (order) => await axios.put(`https://api-v1.niceishfabrics.com/api/order/${order.get('id')}`, order, { headers : headersParam });
 
-// export const deleteOrderApi = async (order) => await axios.delete(`https://api-v1.niceishfabrics.com/api/order/${order}`, { headers : headersParam });
+export const deleteOrderApi = async (order) => await axios.delete(`https://api-v1.niceishfabrics.com/api/order/${order}`, { headers : headersParam });
 
-// export const updateOrderStausApi = async (order) => await axios.put(`https://api-v1.niceishfabrics.com/api/order/updateStatus`, order, { headers : headersParam });
+export const updateOrderStausApi = async (order) => await axios.put(`https://api-v1.niceishfabrics.com/api/order/updateStatus`, order, { headers : headersParam });
 
-// export const loadOrderListApi = async () => await axios.get(`https://api-v1.niceishfabrics.com/api/order/orderDetails/`, { headers: headersParam });
+export const loadOrderListApi = async () => await axios.get(`https://api-v1.niceishfabrics.com/api/order/orderDetails/`, { headers: headersParam });
 
-// export const loadFilterOrdersApi = async (data) => await axios.get(`https://api-v1.niceishfabrics.com/api/order/?status=${data}`, { headers: headersParam });
+export const loadFilterOrdersApi = async (data) => await axios.get(`https://api-v1.niceishfabrics.com/api/order/?status=${data}`, { headers: headersParam });
 
-// export const loadSingleOrdersApi = async (data) => await axios.get(`https://api-v1.niceishfabrics.com/api/order/singleOrder/${data}`);
+export const loadSingleOrdersApi = async (data) => await axios.get(`https://api-v1.niceishfabrics.com/api/order/singleOrder/${data}`);
 
-// export const loadAllTrackersOrderApi = async () => await axios.get(`https://api-v1.niceishfabrics.com/api/logictic/logistics`, { headers: headersParam });
-
-// export const pageChangeApi = async (pageNumber) => await axios.get(`https://apiv2.shiprocket.in/v1/external/orders?page=${pageNumber}`, { headers: headersParam1 });
-
-export const loadOrdersApi = async () => await axios.get(`http://localhost:7000/api/order/`, { headers: headersParam });
-
-export const createOrdersApi = async (order) => await axios.post(`http://localhost:7000/api/order/Create`, order, { headers: headersParam });
-
-export const updateOrderApi = async (order) => await axios.put(`http://localhost:7000/api/order/${order.get('id')}`, order, { headers : headersParam });
-
-export const deleteOrderApi = async (order) => await axios.delete(`http://localhost:7000/api/order/${order}`, { headers : headersParam });
-
-export const updateOrderStausApi = async (order) => await axios.put(`http://localhost:7000/api/order/updateStatus`, order, { headers : headersParam });
-
-export const loadOrderListApi = async () => await axios.get(`http://localhost:7000/api/order/orderDetails/`, { headers: headersParam });
-
-export const loadFilterOrdersApi = async (data) => await axios.get(`http://localhost:7000/api/order/?status=${data}`, { headers: headersParam });
-
-export const loadSingleOrdersApi = async (data) => await axios.get(`http://localhost:7000/api/order/singleOrder/${data}`);
-
-export const loadAllTrackersOrderApi = async () => await axios.get(`http://localhost:7000/api/logictic/logistics`, { headers: headersParam });
+export const loadAllTrackersOrderApi = async () => await axios.get(`https://api-v1.niceishfabrics.com/api/logictic/logistics`, { headers: headersParam });
 
 export const pageChangeApi = async (pageNumber) => await axios.get(`https://apiv2.shiprocket.in/v1/external/orders?page=${pageNumber}`, { headers: headersParam1 });
+
--- src/Redux/APIs/usersApi.js
+++ src/Redux/APIs/usersApi.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("MPCADMIN"));
 
 const headersParam = {
-    "Content-Type": "application/json",
     "Authorization" : `Bearer ${token?.token}`
 };
 
@@ -11,41 +10,24 @@ let params = (new URL(document.location)).searchParams;
 let name = params.get("token");
 
 
-// export const loginUsersApi = async (users) => await axios.post(`https://api-v1.niceishfabrics.com/api/user/login`, users);
+export const loginUsersApi = async (users) => await axios.post(`https://api-v1.niceishfabrics.com/api/user/login`, users);
 
-// export const forgotPasswordApi = async (data) => await axios.post(`https://api-v1.niceishfabrics.com/api/user/forgot_password`, data);
+export const forgotPasswordApi = async (data) => await axios.post(`https://api-v1.niceishfabrics.com/api/user/forgot_password`, data);
 
-// export const resetPasswordApi = async (data) => await axios.post(`https://api-v1.niceishfabrics.com/api/user/reset_password/${name}`, data, data);
+export const resetPasswordApi = async (data) => await axios.post(`https://api-v1.niceishfabrics.com/api/user/reset_password/${name}`, data, data);
 
-// export const ChangePasswordApi = async (data) => await axios.patch(`https://api-v1.niceishfabrics.com/api/user/change-password`, data, { headers: headersParam });
+export const ChangePasswordApi = async (data) => await axios.patch(`https://api-v1.niceishfabrics.com/api/user/change-password`, data, { headers: headersParam });
 
-// export const loadUsersApi = async () => await axios.get(`https://api-v1.niceishfabrics.com/api/user/getall`, { headers: headersParam });
+export const loadUsersApi = async () => await axios.get(`https://api-v1.niceishfabrics.com/api/user/getall`, { headers: headersParam });
 
-// export const registerUserApi = async (user) => await axios.post(`https://api-v1.niceishfabrics.com/api/user/create`,user, { headers: headersParam });
+export const registerUserApi = async (user) => await axios.post(`https://api-v1.niceishfabrics.com/api/user/create`,user, { headers: headersParam });
 
-// export const updateUserApi = async (user) => await axios.patch(`https://api-v1.niceishfabrics.com/api/user/${user.id}`, user, { headers : headersParam });
+export const updateUserApi = async (user) => await axios.patch(`https://api-v1.niceishfabrics.com/api/user/${user.id}`, user, { headers : headersParam });
 
-// export const deleteUserApi = async (user) => await axios.delete(`https://api-v1.niceishfabrics.com/api/user/${user}`, { headers : headersParam });
+export const deleteUserApi = async (user) => await axios.delete(`https://api-v1.niceishfabrics.com/api/user/${user}`, { headers : headersParam });
 
-// export const getUserBYRoleApi = async (role) => await axios.get(`https://api-v1.niceishfabrics.com/api/user/?title=${role}`, { headers: headersParam });
-
-
+export const getUserBYRoleApi = async (role) => await axios.get(`https://api-v1.niceishfabrics.com/api/user/?title=${role}`, { headers: headersParam });
 
 
-export const loginUsersApi = async (users) => await axios.post(`http://localhost:7000/api/user/login`, users);
 
-export const forgotPasswordApi = async (data) => await axios.post(`http://localhost:7000/api/user/forgot_password`, data);
 
-export const resetPasswordApi = async (data) => await axios.post(`http://localhost:7000/api/user/reset_password/${name}`, data, data);
-
-export const ChangePasswordApi = async (data) => await axios.patch(`http://localhost:7000/api/user/change-password`, data, { headers: headersParam });
-
-export const loadUsersApi = async () => await axios.get(`http://localhost:7000/api/user/getall`, { headers: headersParam });
-
-export const registerUserApi = async (user) => await axios.post(`http://localhost:7000/api/user/create`,user, { headers: headersParam });
-
-export const updateUserApi = async (user) => await axios.patch(`http://localhost:7000/api/user/${user.id}`, user, { headers : headersParam });
-
-export const deleteUserApi = async (user) => await axios.delete(`http://localhost:7000/api/user/${user}`, { headers : headersParam });
-
-export const getUserBYRoleApi = async (role) => await axios.get(`http://localhost:7000/api/user/?title=${role}`, { headers: headersParam });
